003c56ddb27fac68b999696efd8397c896a77c02
Prajwal Sapkal <prajwalmsapkal@gmail.com>
2026-08-15 01:31:09 +0530

    the use of ArrayList, Vector, and StringBuffer

---
+++ Assignment-5/Arrays.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = {5, 2, 8, 3, 1, 6};
+
+        // Displaying elements using normal for loop
+        System.out.println("Using for loop:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        System.out.println();
+
+        // Displaying elements using enhanced for loop
+        System.out.println("Using enhanced for loop:");
+        for (int i : numbers) {
+            System.out.print(i + " ");
+        }
+
+        scanner.close();
+    }
+}
